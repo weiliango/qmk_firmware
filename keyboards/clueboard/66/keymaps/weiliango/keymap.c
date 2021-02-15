0914c05dc6e053a5658c5e0b7fe8dc6ebfe2aa6e
weiliango <weiliang.dev@gmail.com>
2021-02-15 15:47:16 -0800

    create hhkb layout

--- keyboards/clueboard/66/keymaps/weiliango/keymap.c
+++ keyboards/clueboard/66/keymaps/weiliango/keymap.c
@@ -17,16 +17,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,                KC_DEL,
   _CAPS,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_NUHS,KC_ENT,
   KC_LSFT,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RO,  KC_RSFT,        KC_UP,
-  KC_LCTL,KC_LALT,KC_LGUI,KC_MHEN,        KC_SPC, KC_SPC,                         KC_HENK,KC_RALT,MO(_FL),KC_RCTL,KC_LEFT,KC_DOWN,KC_RGHT),
+  KC_LCTL,KC_LALT,MO(_FL),KC_MHEN,        KC_SPC, KC_SPC,                         KC_HENK,KC_RALT,MO(_FL),KC_RCTL,KC_LEFT,KC_DOWN,KC_RGHT),
 
   /* Keymap _FL: Function Layer
    */
 [_FL] = LAYOUT(
-  KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,KC_DEL,         KC_VOLU,
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_MUTE,                KC_VOLD,
-  _______,_______,MO(_CL),_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        KC_PGUP,
-  _______,_______,_______,_______,        _______,_______,                        _______,_______,MO(_FL),_______,KC_HOME,KC_PGDN,KC_END),
+  KC_GRV,    KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,   KC_F12, _______,KC_DEL,       KC_VOLU,
+  KC_TAB,    G(KC_Q),   G(KC_W),   G(KC_E),   G(KC_R),   G(KC_T),   G(KC_Y),   G(KC_U),   G(KC_I),   G(KC_O),   G(KC_P),   KC_UP,  KC_RBRC,KC_BSLS,                KC_DEL,
+  _CAPS,     G(KC_A),   G(KC_S),   G(KC_D),   G(KC_F),   G(KC_G),   KC_PAST,   KC_PSLS,   KC_WHOM,   KC_PGUP,   KC_LEFT,   KC_RGHT,KC_NUHS,KC_ENT,
+  G(KC_LSFT),KC_NUBS,   G(KC_Z),   G(KC_X),   G(KC_C),   G(KC_V),   G(KC_B),   KC_PPLS,   KC_PMNS,   KC_END,    KC_PGDN,   KC_DOWN,KC_RO,  KC_RSFT,        KC_UP,
+  G(KC_LCTL),G(KC_LALT),MO(_FL),   KC_MHEN,        KC_SPC, KC_SPC,                         KC_HENK,KC_RALT,MO(_FL),KC_RCTL,KC_LEFT,KC_DOWN,KC_RGHT),
 
   /* Keymap _CL: Control layer
    */
